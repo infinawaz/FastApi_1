ed28a83bd3d77bfa965c32a4e9220eb9658a022b
Md Shahnawaz Alam Khan <shahnawazdgp07@gmail.com>
2024-07-08 16:54:38 +0530

    put with type hints

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
+from typing import Union
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
 
-
-@app.get("/", response_class=HTMLResponse)
+@app.get("/")
 def read_root():
-    with open("index.html", "r") as f:
-        return f.read()
-      
+    return {"Hello": "World"}
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
