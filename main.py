cf4bea3e2ea84f95463587072acec2095e4b857b
Md Shahnawaz Alam Khan <shahnawazdgp07@gmail.com>
2024-07-08 15:53:13 +0530

    html added with main.py changes

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
+app = FastAPI()
+
+
+
+@app.get("/", response_class=HTMLResponse)
+def read_root():
+    with open("index.html", "r") as f:
+        return f.read()
+      
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
